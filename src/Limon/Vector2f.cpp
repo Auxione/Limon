719d838550c2b1e6a9da8820c1cead161c57b963
Cem Zarifoğlu <cem0509@hotmail.com>
2021-04-21 02:48:58 +0300

    Added Vector2f.

---
+++ src/Limon/Vector2f.cpp
@@ -0,0 +1,9 @@
+#include "Vector2f.h"
+
+limon::Vector2f::Vector2f()
+	: x(0)
+	, y(0) {}
+
+limon::Vector2f::Vector2f(float x, float y)
+	: x(x)
+	, y(y) {}
---
+++ src/Limon/Vector2f.h
@@ -0,0 +1,13 @@
+#pragma once 
+
+namespace limon {
+	class Vector2f {
+	public:
+		float x = 0;
+		float y = 0;
+
+	public:
+		Vector2f();
+		Vector2f(float x, float y);
+	};
+}
